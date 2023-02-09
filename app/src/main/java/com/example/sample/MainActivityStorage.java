ed14f6cf3822d050c4287deb0f7d127bd2fe61d1
2021.dhruv.aswani@ves.ac.in <2021.dhruv.aswani@ves.ac.in>
2023-02-09 18:55:26 +0530

    Commit

--- app/src/main/java/com/example/sample/MainActivityStorage.java
+++ app/src/main/java/com/example/sample/MainActivityStorage.java
@@ -7,10 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SearchView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivityStorage extends AppCompatActivity {
 
     private Button button_view;
     private Button button_additem;
@@ -18,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_storage);
         button_view = findViewById(R.id.button_view);
         button_additem = findViewById(R.id.button_additem);
         button_view.setOnClickListener(new View.OnClickListener() {
@@ -38,4 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+
 }
--- app/src/main/java/com/example/sample/MainActivityViewItems.java
+++ app/src/main/java/com/example/sample/MainActivityViewItems.java
@@ -1,13 +1,16 @@
 package com.example.sample;
 
 
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.sample.model.Item;
 
@@ -16,6 +19,8 @@ import java.util.List;
 
 public class MainActivityViewItems extends AppCompatActivity {
     ListView listView;
+
+    ArrayAdapter arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,12 +32,14 @@ public class MainActivityViewItems extends AppCompatActivity {
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,list);
         listView.setAdapter(arrayAdapter);
 
+
+
         MyDbHandler db = new MyDbHandler(MainActivityViewItems.this);
         Item biscuit = new Item();
         biscuit.setItem_id(1);
         biscuit.setItem_name("GoodDay");
         biscuit.setItem_weight("500");
-        db.addItem(biscuit);
+        //db.addItem(biscuit);
 
 //        biscuit.setItem_name("Marie");
 //        biscuit.setItem_weight("1000");
@@ -42,22 +49,49 @@ public class MainActivityViewItems extends AppCompatActivity {
         Item tea = new Item();
         tea.setItem_id(2);
         tea.setItem_name("Red Label");
-        tea.setItem_weight("1000");
-        db.addItem(tea);
+        //tea.setItem_weight("1000");
+        //db.addItem(tea);
 
         Item coffee = new Item();
         coffee.setItem_id(3);
         coffee.setItem_name("Nescafe");
         coffee.setItem_weight("800");
-        db.addItem(coffee);
+        //db.addItem(coffee);
 
         Item chocolate = new Item();
         chocolate.setItem_id(4);
         chocolate.setItem_name("Dairy Milk");
         chocolate.setItem_weight("550");
-        db.addItem(chocolate);
+        //db.addItem(chocolate);
 
 //        db.deleteContact(2); // Delete contact with id 2
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+            public boolean onItemLongClick(AdapterView<?> arg0, View v,
+                                           int index, long arg3) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivityViewItems.this);
+
+                builder.setTitle("Delete")
+                        .setMessage("Do you want to delete this item ?")
+                        .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        })
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                db.deleteContact(arg0.getId());
+
+                            }
+                        }).show();
+
+                AlertDialog dialog = builder.create();
+                return true;
+            }
+        });
 
         List<Item> allItems = db.getAllItems();
         for(Item item: allItems){
@@ -66,6 +100,29 @@ public class MainActivityViewItems extends AppCompatActivity {
 //            Log.d("dbdhruv","Name: " + item.getItem_name());
             list.add(item.getItem_name());
         }
-
     }
+
+
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//
+//        getMenuInflater().inflate(R.menu.menu,menu);
+//
+//        MenuItem menuItem = menu.findItem(R.id.action_search);
+//        SearchView searchView = (SearchView) menuItem.getActionView();
+//        searchView.setQueryHint("Type Something");
+//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String s) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String newText) {
+//
+//                arrayAdapter.getFilter().filter(newText);
+//                return false;
+//            }
+//        });
+//        return super.onCreateOptionsMenu(menu);
+//    }
 }
